b5f68c69b29352cb7444d116714914e0d3f65743
zekro <riho@live.de>
2020-01-30 11:48:49 +0100

    refractored code and add documentation

--- dht11/main.ino
+++ dht11/main.ino
@@ -1,5 +1,3 @@
-// Please use DHT Sensors version 1.3.2!
-// Later versions brake this program for some reason.
 #include <DHT.h>
 #include <DHT_U.h>
 
@@ -13,6 +11,7 @@ E |   | C
   |   |
    ---
     D
+
 02  A   11
 03  B   07
 04  C   04
@@ -26,8 +25,16 @@ E |   | C
 12  D4  06
 */
 
+// This defines the DHT Sensor type.
+// For example: DHT11, DHT13, ...
+#define DHTTYPE DHT11
+
+// Defines the ammount of milli seconds between
+// the draw of each didget on the display output.
 #define TIMING 5
 
+// Digital pins for controlling the
+// sections of a numbe field
 #define P_A    2
 #define P_B    3
 #define P_C    4
@@ -35,19 +42,26 @@ E |   | C
 #define P_E    6
 #define P_F    7
 #define P_G    8
+
+// Digital pins for selecting the
+// number field to set the sections for
 #define P_D1   9
 #define P_D2  10
 #define P_D3  11
 #define P_D4  12
 #define P_DHT 13
 
-#define DHTTYPE DHT11
-
+// Index of the two character fields
+// for C and H
 #define CHAR_C 10
 #define CHAR_H 11
 
 const int SELECTOR_PINS[4] = { P_D1, P_D2, P_D3, P_D4 };
 const int NUMB_PINS[7] = { P_A, P_B, P_C, P_D, P_E, P_F, P_G };
+
+// Matrix which defines which sections
+// of a field must be lit for displaying
+// the right number / character
 const int NUMBER_PATTERNS[12][7] = {
 //    A  B  C  D  E  F  G
     { 1, 1, 1, 1, 1, 1 },    // 0
@@ -68,19 +82,38 @@ const int NUMBER_PATTERNS[12][7] = {
 
 int currSelected = -1;
 int currStatus = 0;
-int dataBuffer = 0;
+int data = 0;
 int specifier = 0;
 
+// Initialization of the DHT sensor
+// handler with the digital pin of the
+// sensor and the type of th sensor
 DHT dht(P_DHT, DHTTYPE);
 
 // --------------------------------------------------------------------------------
 
+/**
+ * Sets the range of specified pins to
+ * the specified pin mode (either OUTPUT or INPUT).
+ * 
+ * pins  : the array of pins to set
+ * cPins : size of the pins array
+ * mode  : the mode to specify
+ */
 void setPinsMode(const int *pins, const size_t &cPins, const uint8_t &mode) {
     for (int i = 0; i < cPins; ++i) {
         pinMode(pins[i], mode);
     }
 }
 
+/**
+ * Selects the specified digit field
+ * on the display (1st, 2nd, ...)
+ * so that the definition of sections
+ * will affect this field next.
+ * 
+ * pos : position of the digit
+ */
 void select(const int &pos) {
     int pin = SELECTOR_PINS[pos];
 
@@ -94,6 +127,15 @@ void select(const int &pos) {
     currSelected = pin;
 }
 
+/**
+ * Selects the defined position of the digit
+ * and then sets the required pins to HIGH so
+ * that the passed numer / character will be
+ * shown on this digit.
+ * 
+ * pos :  position of the digit
+ * numb : the number / character to be displayed
+ */
 void setNumber(const int &pos, const int &numb) {
     select(pos);
     const int *pattern = NUMBER_PATTERNS[numb];
@@ -102,6 +144,13 @@ void setNumber(const int &pos, const int &numb) {
     }
 }
 
+/**
+ * Splits a two-digit integer into two seperate
+ * integers and writes them into the digits array.
+ * 
+ * numb   : number to be split
+ * digits : array reference to write result to
+ */
 void asDigets(const int &numb, int *digits) {
     digits[0] = numb / 10;
     digits[1] = numb % 10;
@@ -109,7 +158,7 @@ void asDigets(const int &numb, int *digits) {
 
 // --------------------------------------------------------------------------------
 
-// the setup routine runs once when you press reset:
+// the setup routine runs once when you press reset
 void setup() {
     setPinsMode(SELECTOR_PINS, sizeof(SELECTOR_PINS) / sizeof(int), OUTPUT);
     setPinsMode(NUMB_PINS, sizeof(NUMB_PINS) / sizeof(int), OUTPUT);
@@ -122,14 +171,16 @@ void setup() {
     Serial.begin(9600);
 }
 
-// the loop routine runs over and over again forever:
+// the loop routine runs over and over again forever
 void loop() {
     if (currStatus == 1500 / (TIMING * 3)) {
-        dataBuffer = dht.readTemperature();
+        data = dht.readTemperature();
         specifier = CHAR_C;
+        Serial.println(data, specifier);
     } else if (currStatus == 4200 / (TIMING * 3)) {
-        dataBuffer = dht.readHumidity();
+        data = dht.readHumidity();
         specifier = CHAR_H;
+        Serial.println(data, specifier);
     } else if (currStatus == 6000 / (TIMING * 3)) {
         currStatus = 0;
     }
@@ -137,7 +188,7 @@ void loop() {
     currStatus++;
 
     int *digits;
-    asDigets(dataBuffer, digits);
+    asDigets(data, digits);
 
     setNumber(1, digits[0]);
     delay(TIMING);
